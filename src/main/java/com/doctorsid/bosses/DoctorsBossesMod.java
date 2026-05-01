83bc9b050919c21be3e185b06854ba0ee94eabbf
DoctorSid <harveylew.i.s.79098@gmail.com>
2026-05-01 17:42:42 +0100

    Create DoctorsBossesMod.java as the main mod class for NeoForge.

---
+++ src/main/java/com/doctorsid/bosses/DoctorsBossesMod.java
@@ -0,0 +1,16 @@
+package com.doctorsid.bosses;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Main mod class for DoctorsBosses.
+ */
+@Mod(DoctorsBossesMod.MODID)
+public class DoctorsBossesMod {
+
+    public static final String MODID = "doctorsbosses";
+
+    public DoctorsBossesMod() {
+        // Initialization code here
+    }
+}
